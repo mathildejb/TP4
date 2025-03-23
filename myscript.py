24b32a4a8d7a4dd3a22099331c8367917625aeba
Mathildejb <mathildejbrown@gmail.com>
2025-03-23 15:49:03 -0400

    2.0

--- myscript.py
+++ myscript.py
@@ -5,5 +5,7 @@ bad_hash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 
 os.system(f"git bisect start {bad_hash} {good_hash}")
 os.system("git bisect run python manage.py test")
+print("Le premier commit défectueux est:")
+os.system("git show --quiet $(git bisect log | grep 'first bad commit' -A 1 | tail -n 1)")
 
 os.sytem("git bisect reset")
